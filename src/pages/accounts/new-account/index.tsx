5a445b571e921438452c674f4f8ab027e4329245
EnriqueSixto01 <enriquesixto95@gmail.com>
2023-04-21 11:03:52 -0500

    fix fontSize total debt

--- src/pages/accounts/new-account/index.tsx
+++ src/pages/accounts/new-account/index.tsx
@@ -1,22 +1,19 @@
 // ** MUI Imports
 import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
+import PaymentWarranty from 'src/views/accounts/new-account-steps/PaymentWarranty'
 
 // ** Custom Components Imports
-import Information from 'src/views/accounts/new-account-steps/information'
-
+// import Information from 'src/views/accounts/new-account-steps/information'
 
 const NewAccount = () => {
-
-
   return (
     <Grid item xs={12}>
-      <Card>
-        New Account header
-      </Card>
+      <Card>New Account header</Card>
       <Card>
         <div>Accoun Stepper</div>
-        <Information/>
+        {/* <Information/> */}
+        <PaymentWarranty />
       </Card>
     </Grid>
   )
