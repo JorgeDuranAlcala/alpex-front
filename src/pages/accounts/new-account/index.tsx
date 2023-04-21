8035d27936b9d6cf71a4eece33ef46286b9a49e0
valeria.ramirez@therocketcode.com <valeria.ramirez@therocketcode.com>
2023-04-21 16:14:07 -0600

    New Account: Comment section

--- src/pages/accounts/new-account/index.tsx
+++ src/pages/accounts/new-account/index.tsx
@@ -1,17 +1,20 @@
+import { useState } from 'react';
+
 // ** MUI Imports
-import Card from '@mui/material/Card'
-import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
 
 // ** Custom Components Imports
-import CommentSection from 'src/views/accounts/new-account-steps/CommentSection'
-import Information from 'src/views/accounts/new-account-steps/information'
+import CommentSection from 'src/views/accounts/new-account-steps/CommentSection';
+import Information from 'src/views/accounts/new-account-steps/information';
 
 
 const NewAccount = () => {
 
+  const [disableComments, setDisableComments] = useState(true)
 
   return (
-    <Grid item xs={12}>
+    <Grid className="new-account" item xs={12}>
       <Card>
         New Account header
       </Card>
@@ -20,7 +23,7 @@ const NewAccount = () => {
         <Information/>
       </Card>
       <Card>
-        <CommentSection/>
+        <CommentSection disable={disableComments}/>
       </Card>
     </Grid>
   )
