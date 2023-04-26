38b562aa03d22603a23d93316eef30c3dbe5e4d7
valeria.ramirez@therocketcode.com <valeria.ramirez@therocketcode.com>
2023-04-26 14:30:43 -0600

    Cambios new accounts vista.

--- src/pages/accounts/new-account/index.tsx
+++ src/pages/accounts/new-account/index.tsx
@@ -5,16 +5,17 @@ import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
 
 // ** Custom Components Imports
-import CommentSection from 'src/views/accounts/new-account-steps/CommentSection';
+import CommentSection from 'src/views/components/new-accounts/CommentSection';
 
-// import Information from 'src/views/accounts/new-account-steps/information';
-import PaymentWarranty from 'src/views/accounts/new-account-steps/PaymentWarranty';
+import Information from 'src/views/accounts/new-account-steps/information';
+
+// import PaymentWarranty from 'src/views/accounts/new-account-steps/PaymentWarranty';
 
 
 const NewAccount = () => {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [disableComments, setDisableComments] = useState(true)
+  const [disableComments, setDisableComments] = useState(false)
 
   return (
     <Grid className="new-account" item xs={12}>
@@ -22,9 +23,8 @@ const NewAccount = () => {
         New Account header
       </Card>
       <Card>
-        <div>Accoun Stepper</div>
-        {/* <Information/> */}
-        <PaymentWarranty />
+        <Information/>
+        {/* <PaymentWarranty /> */}
       </Card>
       <Card>
         <CommentSection disable={disableComments}/>
