2ca13a438b8d99a9cc6c2d5b1d6e8804f7b870c2
isaac grimaldo <isaac.grimaldo@therocketcode.com>
2023-04-26 09:40:31 -0600

    primera version de los servicios de account

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,3 +1,10 @@
+export enum ACCOUNT_INFORMATION_ROUTES {
+  ADD = 'account/information/add',
+  GET = 'account/Information',
+  UPDATE = 'account/Information'
+}
+
 export enum ACCOUNT_ROUTES {
-  ADD = 'account/add'
+  GET_BY_ID = 'account',
+  GET_ALL = 'account/all'
 }
--- src/services/app.alpex.api-getway.ts
+++ src/services/app.alpex.api-getway.ts
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
-export const AppDynamicReApiGateWay = axios.create({
-  baseURL: process.env.APP_ALPEX_API_GATEWAY,
+export const AppAlpexApiGateWay = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_APP_ALPEX_API_GATEWAY,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
   },
-  timeout: 10000,
-  withCredentials: true
+  timeout: 10000
 })
