e9202297af8740aa0889c36523a26adcafa2888d
omarTherocketcode <omar@therocketcode.com>
2023-04-26 13:52:26 -0600

    update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,1 +1,2 @@
-if [ -d "/var/www/new-front-dynamic-copy" ]; then sudo rm -Rf "/var/www/new-front-dynamic-copy"; fi
+if [ -d "/var/www/api-alpex" ]; then sudo rm -Rf "/var/www/api-alpex"; fi
+sudo mkdir /var/www/new-front-dynamic-copy
