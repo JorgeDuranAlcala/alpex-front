57ec857845d7294d4b5d9e09a31eeba4856019c4
omarTherocketcode <omar@therocketcode.com>
2023-04-26 01:13:16 -0600

    update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,5 @@ sudo chmod 777 /var/www/new-front-dynamic-copy
 sudo cp /var/www/.env.front /var/www/new-front-dynamic-copy/.env
 cd /var/www/new-front-dynamic-copy
 node -v
-yarn install
-yarn build
+# yarn install
+# yarn build
