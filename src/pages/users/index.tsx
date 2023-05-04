3931a2f0734e7a2dc796838c31e39aff709c9047
JorgeGallardoRocket <jorge.gallardo@therocketcode.com>
2023-05-04 17:08:15 -0600

    table fix

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,6 +3,7 @@ import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
 import { useState } from 'react'
 import AddUser from 'src/views/users/forms/AddUser'
+
 import Table from 'src/views/users/table'
 
 const Users = () => {
@@ -14,7 +15,7 @@ const Users = () => {
 
   return (
     <Grid item xs={12} sx={{ minHeight: '100%' }}>
-      <Card sx={{ padding: '16px', minHeight: '100%' }}>
+      <Card sx={{ minHeight: '100%', padding: '16px' }}>
         {view === 'add' && <AddUser handleView={handleView} />}
         {view === 'list' && <Table handleView={handleView} />}
       </Card>
