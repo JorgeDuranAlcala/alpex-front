53ac69ccc72f35c29d0218fe358720daf1210f11
RAFALAMAO <aaronjt@outlook.com>
2023-05-17 11:43:53 -0600

    fixed status and not used variable

--- src/views/accounts/Table/Status.tsx
+++ src/views/accounts/Table/Status.tsx
@@ -38,34 +38,41 @@ export enum EStatusString {
 
 const Status: React.FC<IStatusProps> = ({ status }) => {
   const statusComponents: IComponents = {
-    [EStatus.PENDING]: (
+    [EStatusString.PENDING]: (
       <>
-        <Box component='span' sx={{
-          display: 'flex',
-          gap: 2,
-          color: colors.primary.main,
-          alignItems: 'center',
-          overflow: 'hidden'
-        }}>
-          <Icon icon='mdi:clock' fontSize={20} />
-          <Typography sx={{
-            fontSize: fonts.size.px16,
-            fontFamily: fonts.inter,
+        <Box
+          component='span'
+          sx={{
+            display: 'flex',
+            gap: 2,
+            color: colors.primary.main,
             alignItems: 'center',
-            '&.MuiTypography-root': {
-              color: '#174BC1',
-              fontWeight: '500'
-            }
-
-          }}>
+            overflow: 'hidden'
+          }}
+        >
+          <Icon icon='mdi:clock' fontSize={20} />
+          <Typography
+            sx={{
+              fontSize: fonts.size.px16,
+              fontFamily: fonts.inter,
+              alignItems: 'center',
+              '&.MuiTypography-root': {
+                color: '#174BC1',
+                fontWeight: '500'
+              }
+            }}
+          >
             {EStatusString.PENDING}
           </Typography>
         </Box>
       </>
     ),
-    [EStatus.BOUND]: (
+    [EStatusString.BOUND]: (
       <>
-        <Box component='span' sx={{ overflow: 'hidden', display: 'flex', gap: 2, color: colors.primary.light, alignContent: 'center' }}>
+        <Box
+          component='span'
+          sx={{ overflow: 'hidden', display: 'flex', gap: 2, color: colors.primary.light, alignContent: 'center' }}
+        >
           <Icon icon='icon-park-outline:link-three' fontSize={20} />
           <Typography sx={{ color: colors.primary.light, fontSize: fonts.size.px16, fontFamily: fonts.inter }}>
             {EStatusString.BOUND}
@@ -73,9 +80,12 @@ const Status: React.FC<IStatusProps> = ({ status }) => {
         </Box>
       </>
     ),
-    [EStatus.NOT_MATERIALIZED]: (
+    [EStatusString.NOT_MATERIALIZED]: (
       <>
-        <Box component='span' sx={{ overflow: 'hidden', display: 'flex', gap: 2, color: colors.warning.dark, alignContent: 'center' }}>
+        <Box
+          component='span'
+          sx={{ overflow: 'hidden', display: 'flex', gap: 2, color: colors.warning.dark, alignContent: 'center' }}
+        >
           <Icon icon='mdi:progress-helper' fontSize={20} />
           <Typography sx={{ color: colors.warning.dark, fontSize: fonts.size.px16, fontFamily: fonts.inter }}>
             {EStatusString.NOT_MATERIALIZED}
@@ -83,9 +93,12 @@ const Status: React.FC<IStatusProps> = ({ status }) => {
         </Box>
       </>
     ),
-    [EStatus.NOT_TAKEN_UP]: (
+    [EStatusString.NOT_TAKEN_UP]: (
       <>
-        <Box component='span' sx={{ overflow: 'hidden', display: 'flex', gap: 2, color: colors.secondary.dark, alignContent: 'center' }}>
+        <Box
+          component='span'
+          sx={{ overflow: 'hidden', display: 'flex', gap: 2, color: colors.secondary.dark, alignContent: 'center' }}
+        >
           <Icon icon='custom:not-taken-up' fontSize={20} />
           <Typography sx={{ color: colors.secondary.dark, fontSize: fonts.size.px16, fontFamily: fonts.inter }}>
             {EStatusString.NOT_TAKEN_UP}
@@ -93,9 +106,12 @@ const Status: React.FC<IStatusProps> = ({ status }) => {
         </Box>
       </>
     ),
-    [EStatus.DECLINED]: (
+    [EStatusString.DECLINED]: (
       <>
-        <Box component='span' sx={{ overflow: 'hidden', display: 'flex', gap: 2, color: colors.error.dark, alignContent: 'center' }}>
+        <Box
+          component='span'
+          sx={{ overflow: 'hidden', display: 'flex', gap: 2, color: colors.error.dark, alignContent: 'center' }}
+        >
           <Icon icon='mdi:cancel' fontSize={20} />
           <Typography sx={{ color: colors.error.dark, fontSize: fonts.size.px16, fontFamily: fonts.inter }}>
             {EStatusString.DECLINED}
