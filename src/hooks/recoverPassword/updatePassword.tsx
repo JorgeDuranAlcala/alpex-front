07d7e982f0eb293ccf2883fbba9243a75cdaebf6
EnriqueSixto01 <enriquesixto95@gmail.com>
2023-05-18 18:57:39 -0500

    reset pass

--- src/hooks/recoverPassword/updatePassword.tsx
+++ src/hooks/recoverPassword/updatePassword.tsx
@@ -2,8 +2,9 @@ import { PasswordPutDto } from '@/services/recoveryPassword/dtos/RecoveryPassDto
 import recoveryPasswordService from '@/services/recoveryPassword/recoveryPassword.service'
 
 export const useUpdatePassword = () => {
-  const updatePass = async (updatePasswords: Partial<PasswordPutDto>, token: string | (string | null)[] | null) => {
+  const updatePass = async (updatePasswords: Partial<PasswordPutDto>, token: string | string[] | undefined) => {
     const updatePassword = await recoveryPasswordService.updatePassword(updatePasswords, token)
+    console.log(updatePassword)
 
     return updatePassword
   }
