aaad34918d2852686c4dce03dd79da79803841c6
Dennise <erika.dennise.08@gmail.com>
2023-06-05 15:39:20 -0700

    Catalog country and fix cedant contacts

--- src/hooks/catalogs/country/useGetAll.ts
+++ src/hooks/catalogs/country/useGetAll.ts
@@ -5,17 +5,24 @@ import { CountryDto } from 'src/services/catalogs/dtos/country.dto'
 export const useGetAllCountries = () => {
   const [countries, setCountry] = useState<CountryDto[]>([])
 
+  const getAllCountries = async () => {
+    const data = await CountryService.getAll()
+    setCountry(data)
+  }
+
   useEffect(() => {
-    CountryService.getAll()
+    /* CountryService.getAll()
       .then(countries => {
         setCountry(countries)
       })
       .catch(error => {
         throw error
-      })
+      }) */
+    getAllCountries()
   }, [])
 
   return {
-    countries
+    countries,
+    getAllCountries
   }
 }
--- src/views/catalogues/dynamic/countries-currencies/index.tsx
+++ src/views/catalogues/dynamic/countries-currencies/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import Icon from 'src/@core/components/icon'
 
 //customed imports
+import { useAddCountry, useDeleteCountry, useGetAllCountries, useUpdateById } from '@/hooks/catalogs/country'
 import AddEditModal from '@/views/components/modals/add-edit-modal'
 import DeleteModal from '@/views/components/modals/delete-modal'
 
@@ -18,16 +19,14 @@ export interface ICurrencies {
 }
 
 const CountriesCurrencies = () => {
-
   // Handle Data
-  const [countries, setCountries] = useState<ICountries[]>([])
-  const [selectedCountry, setSelectedCountry] = useState<ICountries | null>(null);
-  const [currentCountry, setCurrentCountry] = useState<ICountries | null>(null);
-  const [countryToDelete, setCountryToDelete] = useState<number | undefined>(0)
+  const [selectedCountry, setSelectedCountry] = useState<ICountries | null>(null)
+  const [currentCountry, setCurrentCountry] = useState<ICountries | null>(null)
+  const [countryToDelete, setCountryToDelete] = useState<number>(0)
 
   const [currencies, setCurrencies] = useState<ICurrencies[]>([])
-  const [selectedCurrency, setSelectedCurrency] = useState<ICurrencies | null>(null);
-  const [currentCurrency, setCurrentCurrency] = useState<ICurrencies | null>(null);
+  const [selectedCurrency, setSelectedCurrency] = useState<ICurrencies | null>(null)
+  const [currentCurrency, setCurrentCurrency] = useState<ICurrencies | null>(null)
   const [currencyToDelete, setCurrencyToDelete] = useState<number | undefined>(0)
 
   //Handle Modals
@@ -39,91 +38,121 @@ const CountriesCurrencies = () => {
   const [openEditCountry, setOpenEditCountry] = useState(false)
   const [openDeleteCountry, setOpenDeleteCountry] = useState(false)
 
-  const getCountries = () => { //Call to edit broker service
-    const data: ICountries[] = [
-      { id: 1, name: 'México' },
-      { id: 2, name: 'United States' },
-      { id: 3, name: 'Dominican Republic' },
-      { id: 4, name: 'Brazil' },
-    ]
+  // Handle Alerts
+  const [showAlert, setShowAlert] = useState(true)
+  const [alertType, setAlertType] = useState('')
+  const [alertText, setAlertText] = useState('')
+  const [alertIcon, setAlertIcon] = useState('')
 
-    return data
+  const { deleteCountry: deleteCountryById } = useDeleteCountry()
+  const { saveCountry } = useAddCountry()
+  const { countries, getAllCountries } = useGetAllCountries()
+  const { update } = useUpdateById()
+
+  const triggerAlert = (type: string, text?: string) => {
+    setAlertType(type)
+
+    switch (type) {
+      case 'success':
+        setAlertText(text || 'NEW CONTACT ADDED')
+        setAlertIcon('mdi:check-circle-outline')
+        break
+      case 'error':
+        setAlertText('UNKNOWN ERROR, TRY AGAIN')
+        setAlertIcon('mdi:alert-circle-outline')
+        break
+      case 'warn':
+        setAlertText('NO INTERNET CONNECTION')
+        setAlertIcon('mdi:alert-outline')
+        break
+      default:
+        break
+    }
+
+    setShowAlert(true)
+
+    setTimeout(() => {
+      setShowAlert(false)
+    }, 5000)
   }
 
-  const getCurrencies = () => { //Call to edit broker service
+  useEffect(() => {
+    setCurrencies(getCurrencies())
+  }, [])
+
+  const getCurrencies = () => {
+    //Call to edit broker service
     const data: ICurrencies[] = [
       { id: 1, currency: 'MX' },
       { id: 2, currency: 'USD' },
       { id: 3, currency: 'DOP' },
-      { id: 4, currency: 'BRL' },
+      { id: 4, currency: 'BRL' }
     ]
 
     return data
   }
 
   const handleEditCountry = (country: ICountries) => {
-
     setCurrentCountry(country)
-    setSelectedCountry(null);
+    setSelectedCountry(null)
     setOpenEditCountry(true)
   }
 
-
-  const handleDeleteCountry = (id: number | undefined) => {
-    setCountryToDelete(id);
-    setSelectedCountry(null);
-    setOpenDeleteCountry(true);
+  const handleDeleteCountry = (id: number) => {
+    setCountryToDelete(id)
+    setSelectedCountry(null)
+    setOpenDeleteCountry(true)
   }
 
   const handleEditCurrency = (newCurrency: ICurrencies) => {
-
     setCurrentCurrency(newCurrency)
-    setSelectedCurrency(null);
+    setSelectedCurrency(null)
     setOpenEditCurrency(true)
   }
 
-
   const handleDeleteCurrency = (id: number | undefined) => {
-    setCurrencyToDelete(id);
-    setSelectedCurrency(null);
-    setOpenDeleteCurrency(true);
+    setCurrencyToDelete(id)
+    setSelectedCurrency(null)
+    setOpenDeleteCurrency(true)
   }
 
-
-  const addCountry = (value: string) => {
-    const newCountry = { id: countries.length, name: value }
-    setCountries(prevCountries => [...prevCountries, newCountry]);
+  const addCountry = async (value: string) => {
+    const result = await saveCountry({
+      name: value
+    })
+    if (result) {
+      triggerAlert('success')
+      getAllCountries()
+    }
     setOpenAddCountry(false)
-  };
+  }
 
-  const editCountry = (value: string) => {
+  const editCountry = async (value: string) => {
     if (currentCountry) {
-      setCountries(prevCountries =>
-        prevCountries.map(country => {
-          if (country.id === currentCountry.id) {
-            return { ...country, name: value };
-          }
-
-          return country;
-        })
-      );
+      const result = await update(currentCountry.id, {
+        name: value
+      })
+      if (result) {
+        triggerAlert('success', 'CHANGES SAVED')
+        getAllCountries()
+      }
     }
     setOpenEditCountry(false)
-  };
+  }
 
-  const deleteCountry = () => {
-    setCountries(prevCountries =>
-      prevCountries.filter(country => country.id !== countryToDelete)
-    );
+  const deleteCountry = async () => {
+    const result = await deleteCountryById(countryToDelete)
+    if (result) {
+      //it needs an alert o message
+      console.log('success')
+      getAllCountries()
+    }
     setOpenDeleteCountry(false)
-  };
-
-
+  }
 
   const addCurrency = (value: string) => {
-
     const newCurrency = { id: currencies.length, currency: value }
-    setCurrencies(prevCurrencies => [...prevCurrencies, newCurrency]);
+    setCurrencies(prevCurrencies => [...prevCurrencies, newCurrency])
     setOpenAddCurrency(false)
   }
 
@@ -132,52 +161,42 @@ const CountriesCurrencies = () => {
       setCurrencies(prevCurrencies =>
         prevCurrencies.map(currency => {
           if (currency.id === currentCurrency.id) {
-            return { ...currency, currency: value };
+            return { ...currency, currency: value }
           }
 
-          return currency;
+          return currency
         })
-      );
+      )
     }
     setOpenEditCurrency(false)
   }
 
   const deleteCurrency = () => {
-    setCurrencies(prevCurrencies =>
-      prevCurrencies.filter(currency => currency.id !== currencyToDelete)
-    );
+    setCurrencies(prevCurrencies => prevCurrencies.filter(currency => currency.id !== currencyToDelete))
 
     setOpenDeleteCurrency(false)
   }
 
-
-  // const getCurrencies = () => { //Call to add broker service
-  //   console.log("call add reinsurer service", newReinsurer.id)
-  //   setIsReinsurerSaved(true)
-  // }
-
-  useEffect(() => {
-    setCountries(getCountries)
-    setCurrencies(getCurrencies)
-
-    //eslint-disable-next-line
-  }, [])
-
   return (
     <>
       <div className='country-currencies-wrapper'>
-        <div className="inner-container">
-          <div className="header-block">
+        {/* TODO:  */}
+        {showAlert && (
+          <div className={`${alertType} contacts-alert`}>
+            <div className='btn-icon'>
+              <Icon icon={alertIcon} />
+            </div>
+            {alertText}
+          </div>
+        )}
+        <div className='inner-container'>
+          <div className='header-block'>
             <div className='header-icon'>
               <Icon className='icon' icon='mdi:map-marker' />
             </div>
             <div className='content'>
-              <div className='title'>
-                Countries ({countries.length})
-              </div>
-              <div className='description'>
-                You can add a country by clicking the plus button.
-              </div>
+              <div className='title'>Countries ({countries.length})</div>
+              <div className='description'>You can add a country by clicking the plus button.</div>
             </div>
             <div className='add-btn'>
               <Icon
@@ -188,54 +207,48 @@ const CountriesCurrencies = () => {
               />
             </div>
           </div>
-          <div className="block-list">
-            {countries.map((country) => {
-              const showActions = country === selectedCountry;
+          <div className='block-list'>
+            {countries.map(country => {
+              const showActions = country === selectedCountry
 
               return (
-                <>
-
-                  <div className="list-item">
-                    <div className="item-name" key={country.id}>
-                      {country.name}
-                    </div>
-                    <div className="item-menu" onClick={() => {
+                <div className='list-item' key={country.id}>
+                  <div className='item-name'>{country.name}</div>
+                  <div
+                    className='item-menu'
+                    onClick={() => {
                       if (showActions) {
-                        setSelectedCountry(null);
+                        setSelectedCountry(null)
                       } else {
-                        setSelectedCountry(country);
+                        setSelectedCountry(country)
                       }
                     }}
-                    >
-                      <Icon icon='mdi:dots-vertical' />
-                      {showActions &&
-                        <div className='actions-menu'>
-                          <div className='menu-option' onClick={() => handleEditCountry(country)}>
-                            Edit
-                          </div>
-                          <div className='menu-option' onClick={() => handleDeleteCountry(country.id)}>
-                            Delete
-                          </div>
-                        </div>}
-                    </div>
+                  >
+                    <Icon icon='mdi:dots-vertical' />
+                    {showActions && (
+                      <div className='actions-menu'>
+                        <div className='menu-option' onClick={() => handleEditCountry(country)}>
+                          Edit
+                        </div>
+                        <div className='menu-option' onClick={() => handleDeleteCountry(country.id)}>
+                          Delete
+                        </div>
+                      </div>
+                    )}
                   </div>
-                </>
+                </div>
               )
             })}
           </div>
         </div>
-        <div className="inner-container">
-          <div className="header-block">
+        <div className='inner-container'>
+          <div className='header-block'>
             <div className='header-icon'>
               <Icon className='icon' icon='solar:dollar-minimalistic-bold' />
             </div>
             <div className='content'>
-              <div className='title'>
-                Currencies ({currencies.length})
-              </div>
-              <div className='description'>
-                You can add a currency by clicking the plus button.
-              </div>
+              <div className='title'>Currencies ({currencies.length})</div>
+              <div className='description'>You can add a currency by clicking the plus button.</div>
             </div>
             <div className='add-btn'>
               <Icon
@@ -246,38 +259,36 @@ const CountriesCurrencies = () => {
               />
             </div>
           </div>
-          <div className="block-list">
-            {currencies.map((currency) => {
-              const showActions = currency === selectedCurrency;
+          <div className='block-list'>
+            {currencies.map(currency => {
+              const showActions = currency === selectedCurrency
 
               return (
-                <>
-
-                  <div className="list-item">
-                    <div className="item-name" key={currency.id}>
-                      {currency.currency}
-                    </div>
-                    <div className="item-menu" onClick={() => {
+                <div className='list-item' key={currency.id}>
+                  <div className='item-name'>{currency.currency}</div>
+                  <div
+                    className='item-menu'
+                    onClick={() => {
                       if (showActions) {
-                        setSelectedCurrency(null);
+                        setSelectedCurrency(null)
                       } else {
-                        setSelectedCurrency(currency);
+                        setSelectedCurrency(currency)
                       }
                     }}
-                    >
-                      <Icon icon='mdi:dots-vertical' />
-                      {showActions &&
-                        <div className='actions-menu'>
-                          <div className='menu-option' onClick={() => handleEditCurrency(currency)}>
-                            Edit
-                          </div>
-                          <div className='menu-option' onClick={() => handleDeleteCurrency(currency.id)}>
-                            Delete
-                          </div>
-                        </div>}
-                    </div>
+                  >
+                    <Icon icon='mdi:dots-vertical' />
+                    {showActions && (
+                      <div className='actions-menu'>
+                        <div className='menu-option' onClick={() => handleEditCurrency(currency)}>
+                          Edit
+                        </div>
+                        <div className='menu-option' onClick={() => handleDeleteCurrency(currency.id)}>
+                          Delete
+                        </div>
+                      </div>
+                    )}
                   </div>
-                </>
+                </div>
               )
             })}
           </div>
@@ -285,36 +296,43 @@ const CountriesCurrencies = () => {
         <AddEditModal
           openModal={openAddCurrency}
           onSubmit={addCurrency}
-          onClose={() => { setOpenAddCurrency(false) }}
-          title="Add new Currency"
+          onClose={() => {
+            setOpenAddCurrency(false)
+          }}
+          title='Add new Currency'
           label='New Currency'
           actionBtnText='CREATE'
         />
         <AddEditModal
           openModal={openAddCountry}
           onSubmit={addCountry}
-          onClose={() => { setOpenAddCountry(false) }}
-          title="Add new Country"
+          onClose={() => {
+            setOpenAddCountry(false)
+          }}
+          title='Add new Country'
           label='New Country'
           actionBtnText='CREATE'
         />
         <AddEditModal
           openModal={openEditCurrency}
           onSubmit={editCurrency}
-          onClose={() => { setOpenEditCurrency(false) }}
-          title="Edit Currency"
+          onClose={() => {
+            setOpenEditCurrency(false)
+          }}
+          title='Edit Currency'
           label='Type a Currency'
           actionBtnText='EDIT'
         />
         <AddEditModal
           openModal={openEditCountry}
           onSubmit={editCountry}
-          onClose={() => { setOpenEditCountry(false) }}
-          title="Edit Country"
+          onClose={() => {
+            setOpenEditCountry(false)
+          }}
+          title='Edit Country'
           label='Type a country'
           actionBtnText='EDIT'
         />
-
 
         <DeleteModal
           openModal={openDeleteCurrency}
@@ -332,7 +350,6 @@ const CountriesCurrencies = () => {
           onDelete={deleteCountry}
           textItem='Country'
         />
-
       </div>
     </>
   )
