b1ff6a691c024a31132fe93a315051f3f837f147
omarTherocketcode <omar@therocketcode.com>
2023-06-07 13:20:29 -0600

    UPDATE

--- src/hooks/accounts/security/useUpdateById.tsx
+++ src/hooks/accounts/security/useUpdateById.tsx
@@ -3,7 +3,7 @@ import { SecurityDto } from 'src/services/accounts/dtos/security.dto'
 import SecurityService from 'src/services/accounts/security.service'
 
 export const useUpdateSecurities = () => {
-  const [securities, setSecurities] = useState<SecurityDto[]>()
+  const [securities] = useState<SecurityDto[]>()
 
   const updateSecurities = async (securitiesIn: Partial<SecurityDto>[]) => {
     return await SecurityService.update(securitiesIn)
