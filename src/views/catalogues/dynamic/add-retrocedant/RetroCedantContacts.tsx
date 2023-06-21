dd325e26c3fc1b6a5670a175eae615f7d8b27be6
valeria.ramirez@therocketcode.com <valeria.ramirez@therocketcode.com>
2023-06-21 11:05:04 -0600

    Cambios en estilos y clase de alerts.

--- src/views/catalogues/dynamic/add-retrocedant/RetroCedantContacts.tsx
+++ src/views/catalogues/dynamic/add-retrocedant/RetroCedantContacts.tsx
@@ -130,15 +130,15 @@ const RetroCedantContacts = ({ idRetroCedant }: IRetroCedantContacts) => {
     setAlertType(type)
 
     switch (type) {
-      case 'success':
+      case 'success-alert':
         setAlertText(text || 'NEW CONTACT ADDED')
         setAlertIcon('mdi:check-circle-outline')
         break
-      case 'error':
+      case 'error-alert':
         setAlertText('UNKNOWN ERROR, TRY AGAIN')
         setAlertIcon('mdi:alert-circle-outline')
         break
-      case 'warn':
+      case 'warn-alert':
         setAlertText('NO INTERNET CONNECTION')
         setAlertIcon('mdi:alert-outline')
         break
@@ -355,12 +355,12 @@ const RetroCedantContacts = ({ idRetroCedant }: IRetroCedantContacts) => {
       idCCountry: contactData.idCCountry.id
     })
     if (result) {
-      triggerAlert('success')
+      triggerAlert('success-alert')
       setContactData(initialNewContact)
       getRetroCedantContactsByIdRetroCedant(retroCedantContactsPagination)
     }
     setOpenNewContact(false)
-    triggerAlert('success')
+    triggerAlert('success-alert')
   }
 
   const editContact = async () => {
