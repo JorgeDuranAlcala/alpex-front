88742a8b7dcec924d595ebe99fef8b0f03c49b9e
omarTherocketcode <omar@therocketcode.com>
2023-06-30 00:15:31 -0600

    update

--- src/views/accounts/new-account-steps/Security/components/inputs/SwitchTaxes.tsx
+++ src/views/accounts/new-account-steps/Security/components/inputs/SwitchTaxes.tsx
@@ -1,44 +1,36 @@
-import { SecurityDto } from '@/services/accounts/dtos/security.dto';
-import { RetroCedantDto } from '@/services/catalogs/dtos/RetroCedantDto';
-import { RetroCedantContactDto } from '@/services/catalogs/dtos/retroCedantContact.dto';
-import SwitchAlpex from '@/views/custom/switchs';
-import { FormControl } from '@mui/material';
-import { SetStateAction, useContext } from 'react';
-import { SecurityContext } from '../../SecurityView';
-import { ISecurityInputProps } from '../../interfaces/ISecurityInputProps.interface';
+import { SecurityDto } from '@/services/accounts/dtos/security.dto'
+import SwitchAlpex from '@/views/custom/switchs'
+import { FormControl } from '@mui/material'
+import { SetStateAction, useContext } from 'react'
+import { SecurityContext } from '../../SecurityView'
+import { ISecurityInputProps } from '../../interfaces/ISecurityInputProps.interface'
 
 interface SwitchTaxesProps extends Omit<ISecurityInputProps, 'value' | 'errorMessage'> {
-  isChecked: boolean;
-  security: SecurityDto;
-  setIsTaxesEnabled: (value: SetStateAction<boolean>) => void;
-
+  isChecked: boolean
+  security: SecurityDto
+  setIsTaxesEnabled: (value: SetStateAction<boolean>) => void
 }
 
 export const SwitchTaxes = ({ index, validateForm, security, isChecked, setIsTaxesEnabled }: SwitchTaxesProps) => {
-
-  const {
-    securities,
-    calculateSecurities
-  } = useContext(SecurityContext);
+  const { securities, calculateSecurities } = useContext(SecurityContext)
 
   const handleSwitch = () => {
     console.log({ security })
     const tempSecurities = [...securities]
     tempSecurities[index] = {
       ...tempSecurities[index],
-      idCRetroCedant: {} as RetroCedantDto,
-      idCRetroCedantContact: {} as RetroCedantContactDto,
       taxesActive: !isChecked
 
+      // idCRetroCedant: {} as RetroCedantDto,
+      // idCRetroCedantContact: {} as RetroCedantContactDto,
       // frontingFee: Number(null),
       // frontingFeeAmount: Number(null),
       // frontingFeeActive: !security.frontingFeeActive
     }
-    setIsTaxesEnabled(() => !isChecked);
+    setIsTaxesEnabled(() => !isChecked)
     validateForm(tempSecurities[index])
     calculateSecurities(tempSecurities)
   }
-
 
   return (
     <FormControl fullWidth sx={{ mb: 6 }}>
