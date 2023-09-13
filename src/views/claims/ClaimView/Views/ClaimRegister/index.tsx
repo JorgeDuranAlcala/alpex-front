3a681e2e0ec84b82a8b2cda14bc06a8649ea00bb
AngelesAH <angeles@alimx.mx>
2023-09-13 10:57:50 -0600

    Fixes claim register

--- src/views/claims/ClaimView/Views/ClaimRegister/index.tsx
+++ src/views/claims/ClaimView/Views/ClaimRegister/index.tsx
@@ -7,6 +7,10 @@ import CustomInput from '@/views/claims/ContractView/Components/CustomInput'
 import DatePicker from 'react-datepicker'
 
 const ClaimRegister = () => {
+  const handlechangeValue = (event: KeyboardEvent) => {
+    event.preventDefault()
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={{ xs: 5, sm: 5, md: 5 }}>
@@ -31,6 +35,7 @@ const ClaimRegister = () => {
                 timeIntervals={15}
                 dateFormat='dd/MM/yyyy h:mm aa'
                 placeholderText='DD/MM/YYYY'
+                onChange={() => handlechangeValue}
               />
             </DatePickerWrapper>
           </ContainerInputs>
@@ -51,6 +56,7 @@ const ClaimRegister = () => {
                 timeIntervals={15}
                 dateFormat='dd/MM/yyyy h:mm aa'
                 placeholderText='DD/MM/YYYY'
+                onChange={() => handlechangeValue}
               />
             </DatePickerWrapper>
           </ContainerInputs>
@@ -76,39 +82,18 @@ const ClaimRegister = () => {
             <TitleInputs>Location of Claim</TitleInputs>
             <FormControl fullWidth>
               <InputLabel>Location Name</InputLabel>
-              <Select
-                name='locationName'
-                label='Location Name'
-                defaultValue={''}
-                // value={String(basicInfo.economicSector)}
-                // onChange={handleSelectChange}
-                labelId='location-name'
-              ></Select>
+              <Select name='locationName' label='Location Name' defaultValue={''} labelId='location-name'></Select>
             </FormControl>
             <FormControl fullWidth>
               <TextField autoFocus name='address' label='Address' value={''} />
             </FormControl>
             <FormControl fullWidth>
               <InputLabel>State</InputLabel>
-              <Select
-                name='state'
-                label='State'
-                defaultValue={''}
-                // value={String(basicInfo.economicSector)}
-                // onChange={handleSelectChange}
-                labelId='state'
-              ></Select>
+              <Select name='state' label='State' defaultValue={''} labelId='state'></Select>
             </FormControl>
             <FormControl fullWidth>
               <InputLabel>City</InputLabel>
-              <Select
-                name='city'
-                label='City'
-                defaultValue={''}
-                // value={String(basicInfo.economicSector)}
-                // onChange={handleSelectChange}
-                labelId='city'
-              ></Select>
+              <Select name='city' label='City' defaultValue={''} labelId='city'></Select>
             </FormControl>
             <FormControl fullWidth>
               <InputLabel>Municipality/Province</InputLabel>
@@ -116,8 +101,6 @@ const ClaimRegister = () => {
                 name='municipalityProvince'
                 label='Municipality/Province'
                 defaultValue={''}
-                // value={String(basicInfo.economicSector)}
-                // onChange={handleSelectChange}
                 labelId='municipality-province'
               ></Select>
             </FormControl>
@@ -128,25 +111,11 @@ const ClaimRegister = () => {
             <TitleInputs>Claim Status</TitleInputs>
             <FormControl fullWidth>
               <InputLabel>Status</InputLabel>
-              <Select
-                name='statusClaim'
-                label='Status'
-                defaultValue={''}
-                // value={String(basicInfo.economicSector)}
-                // onChange={handleSelectChange}
-                labelId='status-claim'
-              ></Select>
+              <Select name='statusClaim' label='Status' defaultValue={''} labelId='status-claim'></Select>
             </FormControl>
             <FormControl fullWidth>
               <InputLabel>CESAS</InputLabel>
-              <Select
-                name='cesas'
-                label='CESAS'
-                defaultValue={''}
-                // value={String(basicInfo.economicSector)}
-                // onChange={handleSelectChange}
-                labelId='cesas'
-              ></Select>
+              <Select name='cesas' label='CESAS' defaultValue={''} labelId='cesas'></Select>
             </FormControl>
           </ContainerInputs>
         </Grid>
@@ -155,25 +124,11 @@ const ClaimRegister = () => {
             <TitleInputs>Expert</TitleInputs>
             <FormControl fullWidth>
               <InputLabel>Expert</InputLabel>
-              <Select
-                name='expert'
-                label='Expert'
-                defaultValue={''}
-                // value={String(basicInfo.economicSector)}
-                // onChange={handleSelectChange}
-                labelId='expert'
-              ></Select>
+              <Select name='expert' label='Expert' defaultValue={''} labelId='expert'></Select>
             </FormControl>
             <FormControl fullWidth>
               <InputLabel>Executive</InputLabel>
-              <Select
-                name='executive'
-                label='Executive'
-                defaultValue={''}
-                // value={String(basicInfo.economicSector)}
-                // onChange={handleSelectChange}
-                labelId='executive'
-              ></Select>
+              <Select name='executive' label='Executive' defaultValue={''} labelId='executive'></Select>
             </FormControl>
           </ContainerInputs>
         </Grid>
@@ -182,14 +137,7 @@ const ClaimRegister = () => {
             <TitleInputs>Adjuster</TitleInputs>
             <FormControl fullWidth>
               <InputLabel>Adjuster</InputLabel>
-              <Select
-                name='adjuster'
-                label='Adjuster'
-                defaultValue={''}
-                // value={String(basicInfo.economicSector)}
-                // onChange={handleSelectChange}
-                labelId='adjuster'
-              ></Select>
+              <Select name='adjuster' label='Adjuster' defaultValue={''} labelId='adjuster'></Select>
             </FormControl>
           </ContainerInputs>
         </Grid>
