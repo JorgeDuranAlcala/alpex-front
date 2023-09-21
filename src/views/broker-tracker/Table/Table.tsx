d3e8d22452fd630fd8c51fc780271fe4721ddc67
Jorge Duran <jorgeluis21.duran@gmail.com>
2023-09-21 14:59:21 -0400

    Error var defined but not used in broker-tracker-table

--- src/views/broker-tracker/Table/Table.tsx
+++ src/views/broker-tracker/Table/Table.tsx
@@ -38,203 +38,210 @@ const userThemeConfig: any = Object.assign({}, UserThemeOptions())
 
 const inter = userThemeConfig.typography?.fontFamilyInter
 
-const column: GridColumns<INearlyPaymentStatus> = [
-  {
-    flex: 0.1,
-    field: 'accountId',
-    headerName: 'ACCOUNT ID',
-    minWidth: 150,
-    maxWidth: 150,
-    type: 'string',
-    align: 'left',
-    disableColumnMenu: true,
-    sortable: false,
-    headerClassName: 'account-column-header',
-    renderHeader: ({ colDef }) => {
-      const { headerName } = colDef
-
-      return (
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Typography
-            component={'span'}
-            variant='body1'
-            sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
-          >
-            {headerName}
-          </Typography>
-          <ButtonFilter dataFilter={brokers} insured />
-        </Box>
-      )
-    },
-    renderCell: ({ row }) => (
-      <Typography sx={{ fontSize: fonts.size.px14, fontFamily: fonts.inter }}>{`#${row.accountId}`}</Typography>
-    )
-  },
-  {
-    flex: 0.1,
-    field: 'insured',
-    headerName: 'INSURED',
-    minWidth: 150,
-    maxWidth: 210,
-    type: 'string',
-    align: 'left',
-    disableColumnMenu: true,
-    sortable: false,
-    headerClassName: 'account-column-header',
-    renderHeader: ({ colDef }) => {
-      const { headerName } = colDef
-
-      return (
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Typography
-            component={'span'}
-            variant='body1'
-            sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
-          >
-            {headerName}
-          </Typography>
-          <ButtonFilter dataFilter={brokers} insured />
-        </Box>
-      )
-    },
-    renderCell: ({ row }) => (
-      <Typography
-        variant='body2'
-        sx={{ color: colors.text.light, fontFamily: fonts.inter, fontSize: fonts.size.px14, fontWeight: 400 }}
-      >
-        {row.insured}
-      </Typography>
-    )
-  },
-  {
-    flex: 0.1,
-    field: 'installments',
-    headerName: 'INSTALLMENTS',
-    minWidth: 150,
-    maxWidth: 210,
-    type: 'string',
-    align: 'left',
-    disableColumnMenu: true,
-    sortable: false,
-    headerClassName: 'account-column-header',
-    renderHeader: ({ colDef }) => {
-      const { headerName } = colDef
-
-      return (
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Typography
-            component={'span'}
-            variant='body1'
-            sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
-          >
-            {headerName}
-          </Typography>
-          <ButtonFilter dataFilter={brokers} insured />
-        </Box>
-      )
-    },
-    renderCell: ({ row }) => (
-      <Typography
-        variant='body2'
-        sx={{ color: colors.text.light, fontFamily: fonts.inter, fontSize: fonts.size.px14, fontWeight: 400 }}
-      >
-        {row.installment}
-      </Typography>
-    )
-  },
-  {
-    flex: 0.1,
-    field: 'accountDebt',
-    headerName: 'ACCOUNT DEBT',
-    minWidth: 150,
-    maxWidth: 210,
-    type: 'string',
-    align: 'left',
-    disableColumnMenu: true,
-    sortable: false,
-    headerClassName: 'account-column-header',
-    renderHeader: ({ colDef }) => {
-      const { headerName } = colDef
-
-      return (
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Typography
-            component={'span'}
-            variant='body1'
-            sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
-          >
-            {headerName}
-          </Typography>
-          <ButtonFilter dataFilter={brokers} date />
-        </Box>
-      )
-    },
-    renderCell: ({ row }) => (
-      <Typography
-        variant='body2'
-        sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px14, color: colors.text.light, fontWeight: 400 }}
-      >
-        {row.accountDebt}
-      </Typography>
-    )
-  },
-  {
-    flex: 0.1,
-    field: 'actions',
-    headerName: 'ACTIONS',
-    minWidth: 191,
-    maxWidth: 200,
-    sortable: false,
-    disableColumnMenu: true,
-    cellClassName: 'account-column-cell-pl-0',
-    renderHeader: ({ colDef }) => {
-      const { headerName } = colDef
-
-      return (
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Typography
-            component={'span'}
-            variant='body1'
-            sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
-          >
-            {headerName}
-          </Typography>
-        </div>
-      )
-    },
-    renderCell: ({ row }) => (
-      <div>
-        <Link
-          sx={{ cursor: 'pointer' }}
-          onClick={() => {
-            onAction(row.accountId)
-          }}
-        >
-          <Button
-            variant='text'
-            sx={{
-              width: 'auto',
-              height: '30px',
-              fontSize: '13px',
-              color: userThemeConfig.palette?.buttonText.primary,
-              fontFamily: inter
-            }}
-          >
-            SEE ACCOUNT
-          </Button>
-        </Link>
-      </div>
-
-      // <Button variant='outlined' sx={{ width: '100%', height: '30px', fontSize: '13px' }}>
-      //   Record
-      // </Button>
-    )
-  }
-]
 
 const Table = () => {
   // ** Hooks
-  const router = useRouter()
+  const router = useRouter();
+  
+  const onAction = async (id: number) => {
+    localStorage.setItem('idAccount', String(id))
+    router.push(`/installments/payment-record/?&id=${String(id)}`)
+  }
+  
+
+  const column: GridColumns<INearlyPaymentStatus> = [
+    {
+      flex: 0.1,
+      field: 'accountId',
+      headerName: 'ACCOUNT ID',
+      minWidth: 150,
+      maxWidth: 150,
+      type: 'string',
+      align: 'left',
+      disableColumnMenu: true,
+      sortable: false,
+      headerClassName: 'account-column-header',
+      renderHeader: ({ colDef }) => {
+        const { headerName } = colDef
+  
+        return (
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography
+              component={'span'}
+              variant='body1'
+              sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
+            >
+              {headerName}
+            </Typography>
+            <ButtonFilter dataFilter={brokers} insured />
+          </Box>
+        )
+      },
+      renderCell: ({ row }) => (
+        <Typography sx={{ fontSize: fonts.size.px14, fontFamily: fonts.inter }}>{`#${row.accountId}`}</Typography>
+      )
+    },
+    {
+      flex: 0.1,
+      field: 'insured',
+      headerName: 'INSURED',
+      minWidth: 150,
+      maxWidth: 210,
+      type: 'string',
+      align: 'left',
+      disableColumnMenu: true,
+      sortable: false,
+      headerClassName: 'account-column-header',
+      renderHeader: ({ colDef }) => {
+        const { headerName } = colDef
+  
+        return (
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography
+              component={'span'}
+              variant='body1'
+              sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
+            >
+              {headerName}
+            </Typography>
+            <ButtonFilter dataFilter={brokers} insured />
+          </Box>
+        )
+      },
+      renderCell: ({ row }) => (
+        <Typography
+          variant='body2'
+          sx={{ color: colors.text.light, fontFamily: fonts.inter, fontSize: fonts.size.px14, fontWeight: 400 }}
+        >
+          {row.insured}
+        </Typography>
+      )
+    },
+    {
+      flex: 0.1,
+      field: 'installments',
+      headerName: 'INSTALLMENTS',
+      minWidth: 150,
+      maxWidth: 210,
+      type: 'string',
+      align: 'left',
+      disableColumnMenu: true,
+      sortable: false,
+      headerClassName: 'account-column-header',
+      renderHeader: ({ colDef }) => {
+        const { headerName } = colDef
+  
+        return (
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography
+              component={'span'}
+              variant='body1'
+              sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
+            >
+              {headerName}
+            </Typography>
+            <ButtonFilter dataFilter={brokers} insured />
+          </Box>
+        )
+      },
+      renderCell: ({ row }) => (
+        <Typography
+          variant='body2'
+          sx={{ color: colors.text.light, fontFamily: fonts.inter, fontSize: fonts.size.px14, fontWeight: 400 }}
+        >
+          {row.installment}
+        </Typography>
+      )
+    },
+    {
+      flex: 0.1,
+      field: 'accountDebt',
+      headerName: 'ACCOUNT DEBT',
+      minWidth: 150,
+      maxWidth: 210,
+      type: 'string',
+      align: 'left',
+      disableColumnMenu: true,
+      sortable: false,
+      headerClassName: 'account-column-header',
+      renderHeader: ({ colDef }) => {
+        const { headerName } = colDef
+  
+        return (
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography
+              component={'span'}
+              variant='body1'
+              sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
+            >
+              {headerName}
+            </Typography>
+            <ButtonFilter dataFilter={brokers} date />
+          </Box>
+        )
+      },
+      renderCell: ({ row }) => (
+        <Typography
+          variant='body2'
+          sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px14, color: colors.text.light, fontWeight: 400 }}
+        >
+          {row.accountDebt}
+        </Typography>
+      )
+    },
+    {
+      flex: 0.1,
+      field: 'actions',
+      headerName: 'ACTIONS',
+      minWidth: 191,
+      maxWidth: 200,
+      sortable: false,
+      disableColumnMenu: true,
+      cellClassName: 'account-column-cell-pl-0',
+      renderHeader: ({ colDef }) => {
+        const { headerName } = colDef
+  
+        return (
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography
+              component={'span'}
+              variant='body1'
+              sx={{ fontFamily: fonts.inter, fontSize: fonts.size.px12, color: colors.text.light, fontWeight: 500 }}
+            >
+              {headerName}
+            </Typography>
+          </div>
+        )
+      },
+      renderCell: ({ row }) => (
+        <div>
+          <Link
+            sx={{ cursor: 'pointer' }}
+            onClick={() => {
+              onAction(row.accountId)
+            }}
+          >
+            <Button
+              variant='text'
+              sx={{
+                width: 'auto',
+                height: '30px',
+                fontSize: '13px',
+                color: userThemeConfig.palette?.buttonText.primary,
+                fontFamily: inter
+              }}
+            >
+              SEE ACCOUNT
+            </Button>
+          </Link>
+        </div>
+  
+        // <Button variant='outlined' sx={{ width: '100%', height: '30px', fontSize: '13px' }}>
+        //   Record
+        // </Button>
+      )
+    }
+  ]
 
   return (
     <>
@@ -253,11 +260,8 @@ const Table = () => {
       />
     </>
   )
-}
 
-const onAction = async (id: number) => {
-  localStorage.setItem('idAccount', String(id))
-  router.push(`/installments/payment-record/?&id=${String(id)}`)
+  
 }
 
 export default Table
