047c8bae107475a0e887eaa373c134f7a1ccef08
Jorge Duran <jorgeluis21.duran@gmail.com>
2023-10-04 23:13:25 -0400

    conflict solved

--- src/views/catalogues/claims/expert/index.ts
+++ src/views/catalogues/claims/expert/index.ts
@@ -1,8 +1,4 @@
 import { ExpertDto } from 'src/services/dtos/expert.dto'
 
 
-<<<<<<< HEAD
 export type IExpert = ExpertDto
-=======
-  export type IExpert = ExpertDto
->>>>>>> 2e84e9cadc0ebdd0a8ce0393cb13832f7a41607e
