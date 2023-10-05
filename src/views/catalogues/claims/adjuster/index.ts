596e9465bc1ea32fae872ebe4baafe1e6181747b
Jorge Duran <38388353+JorgeDuranAlcala@users.noreply.github.com>
2023-10-04 22:24:52 -0400

    Update index.ts

--- src/views/catalogues/claims/adjuster/index.ts
+++ src/views/catalogues/claims/adjuster/index.ts
@@ -1,24 +1,3 @@
 import { AdjusterDto } from 'src/services/dtos/adjuster.dto'
-/*export interface IAdjuster {
-    id: string;
-    siglas: string;
-    razonSocial: string;
-    proveedor: string;
-    estado: string;
-    rfc: string;
-    calle: string;
-    noExterior: number;
-    noInterior: number;
-    colonia: string;
-    municipio: string;
-    cp: number;
-    telefono: string;
-    correoContacto: string;
-    nombreContacto: string;
-    contactoReporte: string;
-    fechaContrato: string;
-    observaciones: string;
-}
-*/
 
 export type IAdjuster = AdjusterDto
